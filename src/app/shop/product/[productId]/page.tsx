da52434b74fdbd838f4e9c9715da6ae959845689
Michallesss <mwiemszra@gmail.com>
2024-10-29 21:24:50 +0100

    🧑‍💻 Break it all into components

--- src/app/shop/product/[productId]/page.tsx
+++ src/app/shop/product/[productId]/page.tsx
@@ -1,5 +1,12 @@
-/* 
-https://flowbite.com/blocks/e-commerce/product-overview/
-https://flowbite.com/blocks/e-commerce/product-information/
-https://flowbite.com/blocks/e-commerce/product-review/
-*/
+import Overview from "./_components/Overview";
+import Description from "./_components/Description";
+// import Reviews from "./_components/Reviews";
+
+export default function ProductPage() {
+  return (
+    <>
+      <Overview />
+      <Description />
+    </>
+  );
+}
