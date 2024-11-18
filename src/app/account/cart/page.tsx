b126f26e6c14e43f95d9ae5f4893d066659365a8
Michallesss <mwiemszra@gmail.com>
2024-11-18 20:48:50 +0100

    💄 Cart page design

--- src/app/account/cart/page.tsx
+++ src/app/account/cart/page.tsx
@@ -1,10 +1,26 @@
-// https://flowbite.com/blocks/e-commerce/shopping-cart/
+'use client';
+// React
+import { useState } from "react";
+// Components
+import Cart from "./_components/Cart";
+import Product, { ProductProps } from "./_components/Product";
 
 export default function CartPage() {
+  const [products, setProducts] = useState<ProductProps[]>([
+    {
+      id: 1,
+      name: "PC",
+      image: "https://flowbite.s3.amazonaws.com/blocks/e-commerce/imac-front.svg",
+      price: 5000
+    }
+  ]);
+  const [price, setPrice] = useState<number>(() => products.reduce((acc, product) => acc + product.price * (product.quantity || 1), 0));
+
   return (
-    <div>
-      <h1>Cart Page</h1>
-      <p>This is a protected page</p>
-    </div>
+    <Cart price={price} savings={0.00} pickup={10} tax={3}>
+      {products.map((product, index) => (
+        <Product id={product.id} key={index} name={product.name} image={product.image} price={product.price} quantity={product.quantity} />
+      ))}
+    </Cart>
   );
 }
